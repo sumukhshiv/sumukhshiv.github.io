186f6fa9cc503de6b6ce6005af2ede1c76aa1850
Sumukh Shivakumar <sumukhshiv@berkeley.edu>
2016-09-05 23:17:36 -0700

    new

---
+++ test.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $("#name").fadeIn();
+    $(".border").fadeIn();
+    $("#description").fadeIn("10000");
+    $("#me").fadeIn();
+    $(".text").fadeIn();
+    $("#exp").fadeIn();
+    $(".experience").fadeIn();
+    $(".contact").fadeIn();
+});
